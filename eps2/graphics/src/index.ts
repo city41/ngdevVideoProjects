16c44ccdcf49e4cf6ca6de8a728d2510d519c832
Matt Greer <matt.e.greer@gmail.com>
2023-12-17 07:46:59 -0500

    starting to explore the data

--- eps2/graphics/src/index.ts
+++ eps2/graphics/src/index.ts
@@ -1,7 +1,29 @@
-async function main() {
-  const ss2Data = require("./ss2.json");
+type SS2Data = {
+  frameCountSpeed: number;
+  palettes: Record<number, number[]>;
+  spriteMemory: number[];
+};
 
-  console.log({ ss2Data });
+// spriteMemory is in bytes
+const WORD_SIZE = 2;
+
+async function main() {
+  const ss2Data = require("./ss2.json") as SS2Data;
+
+  for (let i = 0; i < 3; ++i) {
+    // each entry in SCB1 is two words
+    const entry = ss2Data.spriteMemory.slice(
+      i * WORD_SIZE * 2,
+      (i + 1) * WORD_SIZE * 2
+    );
+
+    const tileNumber = ((entry[3] & 0xf0) << 16) | (entry[1] << 8) | entry[0];
+    const palette = entry[2];
+    const verticalFlip = !!(entry[2] & 0x2);
+    const horizontalFlip = !!(entry[2] & 0x1);
+
+    console.log({ tileNumber, palette, verticalFlip, horizontalFlip });
+  }
 }
 
 main()
